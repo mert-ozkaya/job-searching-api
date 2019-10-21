dcea2678eb62f9992e8bbb58bf7488a7d6b010c3
Mert Ozkaya <ozkaya-mert@hotmail.com>
2019-10-22 00:42:24 +0300

    added mongodb connection

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const MongoClient = require('mongodb').MongoClient
 const package = require('./package')
+const settings = require('./mongodbSettings');
 
 const PORT = process.env.port || 3000;
 
@@ -8,9 +10,19 @@ const PORT = process.env.port || 3000;
 
 
 
+  MongoClient.connect(settings.db.url, settings.db.options, function(err, client) {
+    if(err){
+      console.log('Could not connect to database!', err);
+      process.exit(1);
+    }
+
+    console.log('Connected to database.');
+    const db = client.db(settings.db.name);
+
     // create express app and start
     const app = require('./app')
-
+    app.set('DB', db); // set the db connection
     app.listen(PORT, function() {
       console.log('App has started.');
     });
+  });
