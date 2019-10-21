e692d3cb0010a8ab8cb60deb93d8979008e769c8
Mert Ozkaya <ozkaya-mert@hotmail.com>
2019-10-22 00:41:47 +0300

    Added body-parser. Added get company info endpoint

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const bodyParser =require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
 
 
-  app.use(function(req, res, next) {
-    res.send("running")
-  });
+app.use('/companyInfo',require('./modules/company/router'))
 
 module.exports = app;
