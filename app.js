1e35d32224737cebf35255825c53b0c74649fb77
Mert Ozkaya <ozkaya-mert@hotmail.com>
2019-10-23 23:17:40 +0300

    Create register endpoint.

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use(function(req, res, next) {
+  req.data = {};
+  next();
+});
+
 app.use('/companyInfo',require('./modules/company/router'))
+app.use('/register',require('./modules/register/router'))
+
 
 module.exports = app;
