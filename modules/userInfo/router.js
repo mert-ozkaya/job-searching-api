45c8c501efbb530deed2aa807c0620f3a163fc8f
mert ozkaya <40736764+mert-ozkaya@users.noreply.github.com>
2019-11-02 21:35:31 +0300

    Update userInfo routers

--- modules/userInfo/router.js
+++ modules/userInfo/router.js
@@ -2,7 +2,9 @@ const Router = require('express').Router;
 
 const router = Router();
 
+
 router.get('/get', require('./callbacks/get-user-info'))
-router.post('/post', require('./callbacks/post-general-info'))
-router.post('/post',require('./callbacks/post-school-info'))
+router.post('/generalInfo',require('./callbacks/post-general-info'))
+router.post('/schoolInfo',require('./callbacks/post-school-info'))
+//router.post('/post',require('./callbacks/post-school-info'))
 module.exports = router;
