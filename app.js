490f271b72cd09e0c7990236c4f2dd9795cb9249
mert ozkaya <40736764+mert-ozkaya@users.noreply.github.com>
2019-11-02 21:31:22 +0300

    Add userInfo and auth endpoint

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(function(req, res, next) {
   req.data = {};
+  console.log("req.data:  ", req.data)
   next();
 });
 
 app.use('/companyInfo',require('./modules/company/router'))
 app.use('/register',require('./modules/register/router'))
-
-
+app.use('/userInfo',require('./modules/register/middlewares/auth'),require('./modules/userInfo/router'))
+app.use('/auth', require('./modules/register/router'));
 module.exports = app;
