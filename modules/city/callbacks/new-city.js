0d83eda1853c1448a50d71b676eae6ad2da6956a
Mert Ozkaya <40736764+mert-ozkaya@users.noreply.github.com>
2019-12-13 18:44:28 +0300

    Update new-city: update response error and success status. Change aggreagate to findOne with filter

--- modules/city/callbacks/new-city.js
+++ modules/city/callbacks/new-city.js
@@ -1,12 +1,12 @@
 
 module.exports = function(req,res) {
 
-  if(req.body.city_title === undefined) {
-    return res.status(400).send('missing-parameter:city_title')
+  if(req.body.name === undefined) {
+    return res.status(400).send('missing-parameter:city_name')
   }
 
   let data = {
-    title: req.body.city_title
+    title: req.body.name
   }
 
   let collection = req.app.get('DB').collection('cities');
@@ -14,10 +14,10 @@ module.exports = function(req,res) {
   let p = collection.insertOne(data);
 
   p.then(function(result){
-    res.sendStatus(200).send('saved-new-city')
+    res.send('saved-new-city')
   }).catch(function(err){
     console.log(err,"DB kayit basarisiz");
-    res.sendStatus(500).send('db-error');
+    res.status(500).send('db-error');
   });
 
 
