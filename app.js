6ea17d03cf8774f0908dfe604c82a6e5d112e68f
Mert Ozkaya <40736764+mert-ozkaya@users.noreply.github.com>
2019-12-13 18:41:29 +0300

    Upload debug, cors and moment modules

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const express = require('express');
 const bodyParser =require('body-parser');
 const cookieParser = require('cookie-parser');
 const { exec } = require('child_process');
+const cors = require('cors')
 const app = express();
+const debug = require('debug')('FindJob-api:app');
+const moment = require('moment');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser());
 
 app.use(function(req, res, next) {
   req.data = {};
+  next();
+});
+
+// request debugger
+app.use(function(req, res, next) {
+  debug('%s %s - (%s) %s', moment().format(), req.ip, req.method, req.originalUrl);
   next();
 });
 
