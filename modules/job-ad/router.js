4af34f7008489e8f911014bcc345bc096ca0630d
Mert Ozkaya <40736764+mert-ozkaya@users.noreply.github.com>
2019-12-13 18:46:41 +0300

    Update  job-ad\router: add auth middleware and remove company_id query parameter

--- modules/job-ad/router.js
+++ modules/job-ad/router.js
@@ -3,11 +3,11 @@ const multer = require('multer');
 const storage = multer.memoryStorage();
 const send = multer({storage: storage});
 const router = Router();
-
+const _auth = require('../register/middlewares/auth')
 
 
 router.get('/list',require('./callbacks/job-ad-list'))
 router.get('/:ad_id',require('./callbacks/get-job-ad'))
-router.post('/new/:company_id',send.single('ad_photo'), require('./callbacks/new-job-ad'))
+router.post('/new', _auth, send.single('ad_photo'), require('./callbacks/new-job-ad'))
 
 module.exports = router;
