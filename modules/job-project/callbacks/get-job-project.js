c3fc6645b14fe824910b8f5ffbaa24ffa413b12f
Mert Ozkaya <40736764+mert-ozkaya@users.noreply.github.com>
2019-12-16 21:10:32 +0300

    Update get-job-project.js

--- modules/job-project/callbacks/get-job-project.js
+++ modules/job-project/callbacks/get-job-project.js
@@ -1,3 +1,21 @@
-module.exports = [
+const ObjectID = require('mongodb').ObjectID
 
-];
+module.exports = function (req,res){
+
+  var _id = req.params._id
+
+  let filter = {
+    "_id": ObjectID(_id)
+  }
+
+  let collection = req.app.get('DB').collection('jobProject');
+  let p = collection.findOne(filter)
+
+  p.then((result) => {
+    res.send(result)
+  }).catch((err) => {
+    res.sendStatus(500).send('db-error');
+  })
+
+
+}
