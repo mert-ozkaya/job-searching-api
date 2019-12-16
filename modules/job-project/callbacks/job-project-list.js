a0cc5ed7c12860fa9e7737681b78d01040d9c9fe
Mert Ozkaya <40736764+mert-ozkaya@users.noreply.github.com>
2019-12-16 21:11:14 +0300

    Updat job-project-list: add calculate remaining time for project

--- modules/job-project/callbacks/job-project-list.js
+++ modules/job-project/callbacks/job-project-list.js
@@ -1,18 +1,70 @@
 const ObjectID = require('mongodb').ObjectID
 
-module.exports = function(req,res){
+module.exports = [
+  getProjectList,
+  calculateRemaningTime
 
-  let pipeline = []
+]
+
+function getProjectList(req,res,next){
+
+  let pipeline = [{
+    '$lookup': {
+      'from': 'register',
+      'localField': 'user_id',
+      'foreignField': '_id',
+      'as': 'company'
+    }
+  },
+  {
+      '$unwind':{
+        'path': '$company'
+      }
+  }]
 
   let collection = req.app.get('DB').collection('jobProject');
   let cursor = collection.aggregate(pipeline)
   let p = cursor.toArray();
 
   p.then((result) => {
-    res.send(result)
+    req.data.result = result
+      next()
   }).catch((err) => {
     res.sendStatus(500).send('db-error');
   })
 
+}
 
+
+function calculateRemaningTime(req,res) {
+
+  for(var i = 0; i<req.data.result.length; i++)
+  {
+    var date1 = new Date(req.data.now.format());
+    var date2 = new Date(req.data.result[i].due_date);
+    //console.log("date1",date1.getTime())
+    //console.log("date2",date2.getTime())
+    let subMiliSeconds = date2.getTime() - date1.getTime()
+    var delta = Math.abs(subMiliSeconds) / 1000;
+
+    // calculate (and subtract) whole days
+    var days = Math.floor(delta / 86400);
+    delta -= days * 86400;
+
+    // calculate (and subtract) whole hours
+    var hours = Math.floor(delta / 3600) % 24;
+    delta -= hours * 3600;
+
+    // calculate (and subtract) whole minutes
+    var minutes = Math.floor(delta / 60) % 60;
+    delta -= minutes * 60;
+
+    // what's left is seconds
+    var seconds = delta % 60;
+
+    req.data.result[i].remaning_time = days + " gün " + hours + " saat " + minutes + " dakika " + seconds + " saniye"
+
+  }
+
+  res.send(req.data.result)
 }
